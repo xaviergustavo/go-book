9d48c1489d74850785cabac4d948d40de2e58817
Gustavo Xavier <gustavoxavier94@icloud.com>
2015-12-27 03:50:24 -0200

    original fetchall program to be changed

---
+++ ch1/1.10/fetchall.go
@@ -0,0 +1,43 @@
+// Exercise 1.10
+// Find a web site that produces a large ammount of data. Investigate
+// caching by running fetchall twice in succession to see whether the
+// reported time changes much. Do you get the same content each time?
+// Modify fetchall to print its output to a file so it can be examined.
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // start a goroutine
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // receive from channel ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channlel ch
+		return
+	}
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
