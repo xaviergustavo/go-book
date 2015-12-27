334ef2887e25ec8e2b561bf1e55053e85fac2327
Gustavo Xavier <gustavoxavier94@icloud.com>
2015-12-27 03:20:27 -0200

    now using io.Copy instead of ioutil.ReadAll

--- ch1/1.7/fetch.go
+++ ch1/1.7/fetch.go
@@ -7,7 +7,7 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
 )
@@ -19,12 +19,10 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
+		_, err = io.Copy(os.Stdout, resp.Body)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
 	}
 }
