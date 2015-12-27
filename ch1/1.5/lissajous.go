a5b023c682c2e8610a5a0ca9ff661ce92c1d6c22
Gustavo Xavier <gustavoxavier94@icloud.com>
2015-12-27 02:31:58 -0200

    added the green on black palette

--- ch1/1.5/lissajous.go
+++ ch1/1.5/lissajous.go
@@ -15,11 +15,11 @@ import (
 	"os"
 )
 
-var palette = []color.Color{color.White, color.Black}
+var palette = []color.Color{color.Black, color.RGBA{0x00, 0xff, 0x00, 0xdd}}
 
 const (
-	whiteIndex = 0 // first color in palette
-	blackIndex = 1 // next color in palette
+	blackIndex = 0 // next color in palette
+	greenIndex = 1 // first color in palette
 )
 
 func main() {
@@ -43,7 +43,7 @@ func lissajous(out io.Writer) {
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), greenIndex)
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
